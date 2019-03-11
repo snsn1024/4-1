a0e7041ed7eb222e642943c0c64d7d05c83c59f4
snsn1024 <47966782+snsn1024@users.noreply.github.com>
2019-03-11 23:50:00 +0900

    [finish]4-2

--- app/controllers/blogs_controller.rb
+++ app/controllers/blogs_controller.rb
@@ -6,17 +6,8 @@ class BlogsController < ApplicationController
   end
 
   def new
-  	@blog = Blog.new
-  end
-
-  def create
   end
 
   def edit
-  end
-
-  private
-  def blog_params
-  	params.require(:blog).permit(:title, :category, :body)
   end
 end
